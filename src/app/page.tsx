d9509d2fdb497d199423ceb3d27e5bc7b337bedf
Cole <colesc@gmail.com>
2024-02-23 22:40:08 -0800

    fix lint issue with apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div>
-          <p className="text-2xl font-bold">Hi, I'm Cole Stites-Clayton</p>
+          <p className="text-2xl font-bold">{"Hi, I'm Cole Stites-Clayton"}</p>
           <p> 
             This website is currently under construction. Please check back
             soon!

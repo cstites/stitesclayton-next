a9100703d53000a05d98ca00e59e54876370e062
Cole <colesc@gmail.com>
2024-02-24 10:41:06 -0800

    feat: add basic social links and cv download and intro paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,52 @@
 import React from "react";
+import { FiGithub, FiLinkedin, FiMail } from "react-icons/fi";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div>
+    <main className="flex min-h-screen flex-col items-start font-mono p-24">
+      <div className=" w-full items-start justify-between  text-sm lg:flex">
+        <div className="mb-4">
           <p className="text-2xl font-bold">{"Hi, I'm Cole Stites-Clayton"}</p>
-          <p> 
-            This website is currently under construction. Please check back
-            soon!
+          <p>
+            This website is still being built, check back soon for more updates!
           </p>
+          <div className="flex text-2xl pt-4 items-start">
+            <a
+              href="https://www.linkedin.com/in/cole-stites-clayton/"
+              className="pr-4"
+            >
+              <FiLinkedin />
+            </a>
+            <a href="https://github.com/cstites" className="pr-4">
+              <FiGithub />
+            </a>
+            <a
+              href="mailto:hello@stitesclayton.co"
+              className="pr-4"
+            >
+              <FiMail />
+            </a>
+          </div>
         </div>
+        <div className="text-xl border-solid border-2 rounded-md max-w-48 p-2">
+          <a className="p-4" href="/resume.pdf" download>
+            Download CV
+          </a>
+        </div>
+      </div>
+
+      <div className="max-w-5xl mt-8 items-start">
+        <p>
+          {" "}
+          I'm an entrepreneur and engineer passionate about sustainability and
+          the natural world. I started{" "}
+          <a href="www.shyftpower.com" className="underline">
+            SHYFT Power Solutions
+          </a>{" "}
+          to help advance the transition to renewable energy in Africa. I am now
+          taking a career break to work on personal projects, build my
+          engineering skills and find my next challenge.
+        </p>
       </div>
     </main>
   );
